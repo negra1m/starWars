dea371897199fbb87676e5658ba9012f2d12b470
Vinicius Negrao de Menezes <vnsn_@outlook.com>
2019-11-27 00:02:07 -0300

    Feat: Starting Fix Styes

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -10,25 +10,8 @@ import { Component, OnInit } from '@angular/core';
 // App component will serve for render the route outlet component
 
 export class AppComponent implements OnInit {
-  title = 'star-wars';
-  audio = new Audio();
-  iconName = '2';
+
   ngOnInit() {
-    this.playAudio();
   }
 
-  playAudio() {
-    this.audio.src = encodeURIComponent('assets/starwars.mp3');
-    this.audio.load();
-    this.audio.play();
-    this.audio.volume = 0.1;
-  }
-
-  muteUnmute() {
-    this.audio.volume ? (this.audio.volume = 0, this.iconName = 'x') : (this.audio.volume = 0.1, this.iconName = '2');
-  }
-
-  skip() {
-    // TODO
-  }
 }
--- src/app/modules/intro/components/header/header.component.ts
+++ src/app/modules/intro/components/header/header.component.ts
@@ -6,10 +6,31 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./header.component.sass']
 })
 export class HeaderComponent implements OnInit {
+  title = 'star-wars';
+  audio = new Audio();
+  iconName = '2';
 
-  constructor() { }
+  constructor() {
+    // If needed services integration later
+  }
 
   ngOnInit() {
+    this.playAudio();
+  }
+
+  playAudio() {
+    this.audio.src = encodeURIComponent('assets/starwars.mp3');
+    this.audio.load();
+    this.audio.play();
+    this.audio.volume = 0.1;
+  }
+
+  muteUnmute() {
+    this.audio.volume ? (this.audio.volume = 0, this.iconName = 'x') : (this.audio.volume = 0.1, this.iconName = '2');
+  }
+
+  skip() {
+    // TODO
   }
 
 }
