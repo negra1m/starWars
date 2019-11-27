618fb975faf7f61394afff7418977da95a978f4c
Vinicius Negrao de Menezes <vnsn_@outlook.com>
2019-11-26 23:37:39 -0300

    Feat: Adding intro and intro-container component

---
+++ src/app/modules/intro/components/intro-container/intro-container.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-intro-container',
+  templateUrl: './intro-container.component.html',
+  styleUrls: ['./intro-container.component.sass']
+})
+export class IntroContainerComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit() {
+  }
+
+}
