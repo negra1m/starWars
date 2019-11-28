b82ef0a6e096a389a768f305640b3549832a9526
Vinicius Negrao de Menezes <vnsn_@outlook.com>
2019-11-28 20:17:56 -0300

    Feat: Placing cards

--- src/app/modules/main-page/components/panel/panel.component.ts
+++ src/app/modules/main-page/components/panel/panel.component.ts
@@ -8,8 +8,20 @@ import { Router, ActivatedRoute, ParamMap } from "@angular/router";
 })
 export class PanelComponent implements OnInit {
   choice$: any;
+  selection: any;
+  options = [{}];
+  title = "Choose a category";
+
   constructor(private route: ActivatedRoute) {
     this.choice$ = this.route.snapshot.paramMap.get("side");
+    this.options = [
+      { category: "Movies", icon: "film" },
+      { category: "Characters", icon: "smile" },
+      { category: "Planets", icon: "globe" },
+      { category: "Species", icon: "book-open" },
+      { category: "Vehicles", icon: "truck" },
+      { category: "Starships", icon: "send" }
+    ];
   }
 
   ngOnInit() {}
